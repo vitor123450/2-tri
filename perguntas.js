bf02613bb448166e04f771ab6253723a7dd47ac2
vitor123450 <vitor.henrique.souza23@escola.pr.gov.br>
2026-06-22 11:47:58 -0300

    2tri

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,36 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'História',
+    'Em que ano ocorreu a Independência do Brasil?',
+    'A Independência do Brasil ocorreu em 1822'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Biologia',
+    'Qual é a função das mitocôndrias na célula?',
+    'As mitocôndrias são responsáveis pela respiração celular e geração de energia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Química',
+    'Qual é o elemento químico representado pelo símbolo "O"?',
+    'O símbolo "O" representa o Oxigênio'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Matemática',
+    'O que é um número primo?',
+    'É um número maior que 1 que só pode ser dividido por 1 e por ele mesmo'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a primeira lei de Newton?',
+    'É a Lei da Inércia, que diz que um corpo tende a manter seu estado de repouso ou movimento a menos que uma força atue sobre ele'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é o autor da famosa frase "Penso, logo existo"?',
+    'A frase foi dita pelo filósofo francês René Descartes'
 )
 
